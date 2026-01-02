64c630afb9311643390d80902c32b46448132b6f
[vinald] <okiror1vinald@gmail.com>
2026-01-02 09:15:06 +0300

    project restructure

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
-from app.api.v1.routes import user, blog
+from .api.v1.routes import user, blog
+from .core.database import engine, Base
 
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 version = 'v1.0'
 
-app.include_router(blog.blog_route, prefix=f"/api/{version}/blogs")
-app.include_router(user.user_route, prefix=f"/api/{version}/users")
+app.include_router(blog.blog_route, prefix=f"/api/{version}")
+app.include_router(user.user_route, prefix=f"/api/{version}")
