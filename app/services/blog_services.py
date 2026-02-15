655e7a2dea03e99c3fb228671a569cd2f1671aa6
[vinald] <okiror1vinald@gmail.com>
2026-02-15 19:50:54 +0300

    blog services

--- app/services/blog_services.py
+++ app/services/blog_services.py
@@ -1,9 +1,10 @@
 from app.schemas.blog import BlogPost
+from app.models.blog import Blog
 from sqlalchemy.orm import Session
 
 
 def create_blog(blog_data: BlogPost, db: Session):
-    new_blog = BlogPost(title=blog_data.title, content=blog_data.content)
+    new_blog = Blog(title=blog_data.title, content=blog_data.content)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
@@ -11,15 +12,15 @@ def create_blog(blog_data: BlogPost, db: Session):
 
 
 def get_all_blogs(db: Session):
-    return db.query(BlogPost).all()
+    return db.query(Blog).all()
 
 
 def get_blog_by_id(blog_id: int, db: Session):
-    return db.query(BlogPost).filter(BlogPost.id == blog_id).first()
+    return db.query(Blog).filter(Blog.id == blog_id).first()
 
 
 def update_blog(blog_id: int, blog_data: BlogPost, db: Session):
-    blog = db.query(BlogPost).filter(BlogPost.id == blog_id).first()
+    blog = db.query(Blog).filter(Blog.id == blog_id).first()
     if blog:
         blog.title = blog_data.title
         blog.content = blog_data.content
@@ -30,7 +31,9 @@ def update_blog(blog_id: int, blog_data: BlogPost, db: Session):
 
 
 def delete_blog(blog_id: int, db: Session):
-    blog = db.query(BlogPost).filter(BlogPost.id == blog_id).first()
+    blog = db.query(Blog).filter(Blog.id == blog_id).first()
     if blog:
         db.delete(blog)
         db.commit()
+        return True
+    return False
